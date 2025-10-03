d0b0c8fec43ecdb475d5be61a7f0cdc4c41c429f
Spyros Paterakis <spaterakis@novibet.com>
2025-10-03 08:34:41 +0300

    receive input in user component

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../../dummy-users';
 
@@ -12,14 +12,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input() name!: string;
+  @Input() avatar!: string;
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+  get imageUrl() {
+    return `assets/images/${this.avatar}`;
   }
 
-  onSelectedUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
-  }
+  onSelectedUser() {}
 }
