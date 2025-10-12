7190722da9e569cc7b4013a29ff7e9383222872e
Spyros Paterakis <s.paterakis@outlook.com.gr>
2025-10-12 21:57:53 +0300

    render user name in tasks component based on selected user

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -3,18 +3,23 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 
 import { DUMMY_USERS } from '../dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUsername: string = "";
 
   onSelectUser(id: string) {
     console.log(`Selected user with id ${id}`);
+    const selectedUser = DUMMY_USERS.find(user => user.id == id);
+    
+    this.selectedUsername = selectedUser?.name ?? "";
   }
 }
