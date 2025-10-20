dfaa92c142bd2ae3e888c8698d9731c339abf023
Spyros Paterakis <spaterakis@novibet.com>
2025-10-20 16:39:45 +0300

    introduce two way binding with ngModel directive

--- 01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Output() cancelAddTask = new EventEmitter<void>();
+
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
   
   onCancel() {
     this.cancelAddTask.emit();
