f12ed39d734a519c6a1ece7792c3d9bbb9189ff4
Spyros Paterakis <spaterakis@novibet.com>
2025-10-20 15:58:43 +0300

    close add new task dialog upon backdrop click or cancel button

--- 01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-task',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-
+  @Output() cancelAddTask = new EventEmitter<void>();
+  
+  onCancel() {
+    this.cancelAddTask.emit();
+  }
 }
