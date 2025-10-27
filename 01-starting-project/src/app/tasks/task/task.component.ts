b8fb0448d416236ee2243a643d21a212f37ee54f
Spyros Paterakis <spaterakis@novibet.com>
2025-10-27 14:49:12 +0200

    migrate standalone components to ng modules

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,13 +1,9 @@
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
 import { Task } from './task.model';
-import { CardComponent } from "../../shared/card/card.component";
 import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  standalone: true,
-  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
---
+++ 01-starting-project/src/app/tasks/tasks.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks.component';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [TasksComponent, TaskComponent, NewTaskComponent],
+  exports: [TasksComponent],
+  imports: [CommonModule, FormsModule, SharedModule],
+})
+export class TasksModule {}
